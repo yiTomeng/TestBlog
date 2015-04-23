7bb79b1ea804080a0bc68eca9b3bc88acb2e1e36
yly <ylyyanleyi@gmail.com>
2015-04-23 14:00:42 +0900

    assembly

---
+++ C/nullEff_linux2.6-18RH/null.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	/* test 1
+	int *a;
+	int b = 1;
+	if (!a)
+	{
+		b = 2;
+	}
+	
+	if (a == NULL)
+	{
+		b = 3;
+	}
+	*/
+	
+	/*test 2 3 4 5
+	int a = 1;
+	int b = 2;
+	int *c = &a;
+	int d = 3;
+	int *e = &d;
+	*/
+	
+	/* test 6
+	int a = 1;
+	
+	int b = 2;
+	
+	char c = 's';
+	
+	float d = 1.0;
+	*/
+	
+	/* test 7*/
+	int a = 1;
+	
+	int b = 2;
+	
+	char c[4] = {'0'};
+	
+	float d = 1.0;
+	
+	printf("0xffffffec:%d\n", 0xffffffec);
+	printf("0xfffffff0:%d\n", 0xfffffff0);
+	return 0;
+}
